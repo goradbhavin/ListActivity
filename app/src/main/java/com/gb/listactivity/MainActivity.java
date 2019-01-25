9d8c35041ee0496089f1284598da7af57c77ff43
goradbhavin <goradbhavin25@gmail.com>
2019-01-25 10:29:47 +0530

    Initial commit

---
+++ app/src/main/java/com/gb/listactivity/MainActivity.java
@@ -0,0 +1,39 @@
+package com.gb.listactivity;
+
+import android.app.ListActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends ListActivity {
+
+    String stringArray[] = {"Audi","Benz","BMW","Ford","Honda","Toyoto","Breza"};
+
+    ListView listView;
+    Toolbar toolbar;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("ListActivity");
+        listView = getListView();
+        ArrayAdapter<String> arrayAdapter=new ArrayAdapter<String>(this,R.layout.item_row,R.id.txtItemName,stringArray);
+        listView.setAdapter(arrayAdapter);
+
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String name = (String) getListView().getAdapter().getItem(position);
+        String selectedItem = (String) getListView().getItemAtPosition(position);
+
+        Toast.makeText(this,selectedItem,Toast.LENGTH_LONG).show();
+    }
+}
